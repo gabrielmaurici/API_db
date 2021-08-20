fafb559df431def4cceb3ee6169f36923944b150
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-08-20 09:13:44 -0300

    Add Projeto Data - Models, Context, Repository

---
+++ API_Prod/Data/Context/ProdutoContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    class ProdutoContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public ProdutoContext() : base("Data Source = 192.168.0.163; Initial Catalog = Companies; Persist Security Info=True;User ID = Companies; Password=HavanLabs2@")
+        {
+
+        }
+    }
+}
---
+++ API_Prod/Data/Repository/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using Data.Context;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public abstract class BaseRepository<T> where T : Base
+    {
+        public virtual void Create(T model)
+        {
+            using (var context = new ProdutoContext())
+            {
+                context.Set<T>().Add(model);
+                context.SaveChanges();
+            }
+        }
+        public virtual List<T> Read()
+        {
+            List<T> lista = new List<T>();
+            using (var context = new ProdutoContext())
+            {
+                context.Set<T>().ToList<T>();
+            }
+            return lista;
+        }
+        public virtual T Read(int id)
+        {
+            T model = Activator.CreateInstance<T>();
+            using (var context = new ProdutoContext())
+            {
+                model = context.Set<T>().Find(id);
+            }
+            return model;
+        }
+        public virtual void Update(T model)
+        {
+            using (var context = new ProdutoContext())
+            {
+                context.Entry<T>(model).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public virtual void Delete(int id)
+        {
+            using (var context = new ProdutoContext())
+            {
+                context.Entry<T>(Read(id)).State = System.Data.Entity.EntityState.Deleted;
+            }
+        }
+    }
+}
